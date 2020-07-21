ca418a6864f213c88129a36a83067425087bbc18
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-07-20 22:25:20 -0500

    [change] the function, malloc to calloc

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 		return (NULL);
-	new_bt = malloc(sizeof(binary_tree_t));
+	new_bt = calloc(1, sizeof(binary_tree_t));
 	if (new_bt == NULL)
 		return (NULL);
 	new_bt->n = value;
