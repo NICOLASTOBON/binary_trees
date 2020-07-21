f3acef6c7f20374204ffd75677065b798523fe61
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-07-20 22:24:29 -0500

    [change] the function, malloc to calloc

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,7 +9,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_bt = NULL;
 
-	new_bt = malloc(sizeof(binary_tree_t));
+	new_bt = calloc(1, sizeof(binary_tree_t));
 	if (new_bt == NULL)
 		return (NULL);
 
